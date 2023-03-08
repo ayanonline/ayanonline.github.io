7403f9a473e38c547c63ffc64eb4e1ac15144419
ayan <ayanghosh1389@gmail.com>
2023-03-09 00:01:47 +0530

    typewriter effect added to name

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let bar = document.querySelector("#bar");
 let bar2 = document.querySelector("#closeBar");
 let resNav = document.querySelector(".res-nav");
 let navBar = document.querySelector(".nav-bar");
+const title = document.querySelector("#ayan");
+const my_name = " Ayan";
+let index = 1;
 bar.addEventListener("click", () => {
   resNav.classList.remove("hide");
   navBar.classList.add("hide");
@@ -10,3 +13,14 @@ bar2.addEventListener("click", () => {
   navBar.classList.remove("hide");
   resNav.classList.add("hide");
 });
+
+//type writer effect
+const typeWriter = () => {
+  let new_title = my_name.slice(0, index);
+  title.innerText = new_title;
+  index > my_name.length ? (index = 1) : index++;
+
+  setTimeout(() => typeWriter(), 400);
+};
+
+typeWriter();
